63c4cc165fbb7ea124ffbbb0afd43f8414ed1433
GEARdHAX <81669068+GEARdHAX@users.noreply.github.com>
2023-05-24 21:40:26 +0530

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,44 @@
+import mysql.connector
+# Replace 'host', 'database', 'user', and 'password' with your database credentials
+conn = mysql.connector.connect(
+    host='localhost',
+    database='world',
+    user='root',
+    password='1234'
+)
+if conn.is_connected():
+    print('\nSuccessfully connected to Database\n')
+else:
+    print('\nFailed to connect to Database\n')
+
+cursor = conn.cursor()
+
+n = int(input("Enter the number of values to insert: "))
+
+for i in range(n):
+    name = input("Enter name: ")
+    phnum = input("Enter phone number: ")
+    city = input("Enter city: ")
+    income = int(input("Enter income: "))
+
+    # SQL query to insert values into the 'users' table
+    query = "INSERT INTO users (name, phnum, city, income) VALUES (%s, %s, %s, %s)"
+    values = (name, phnum, city, income)
+
+    # Execute the query
+    cursor.execute(query, values)
+
+    # Commit the changes to the database
+    conn.commit()
+
+    print("Record inserted successfully.")
+
+query2 = "SELECT * FROM users"
+
+cursor.execute(query2)
+rows = cursor.fetchall()
+for i in rows:
+    print(i)
+# Close the cursor and the connection
+cursor.close()
+conn.close()
